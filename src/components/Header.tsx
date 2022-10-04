a3e9b8b7de8a03e52974a0f78fa9e2be02452dcf
Ceren Demirci <cerdemrc@gmail.com>
2022-10-04 23:01:27 +0300

    feat: add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import '../styles/Header.scss';
+
+const Header: React.FC = () => {
+	return (
+		<div className='header'>
+			<span className='header-first'>Quiz</span>
+			Crack
+			<span className='header-underline'>-----</span>
+		</div>
+	);
+};
+
+export default Header;
